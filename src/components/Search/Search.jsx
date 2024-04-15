aa8604fe0e721a40b5765992875c885153c59ccd
deankago <martokhago@gmail.com>
2024-04-15 15:38:48 -0400

    solve errors and test calculate age functionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,73 +1,97 @@
-import React, { useState } from "react";
-import "./search.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Image } from "react-bootstrap";
-import Welcome from "../../images/welcome.jpg";
+import React, { useState } from 'react';
+import './search.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Image } from 'react-bootstrap';
+import Welcome from '../../images/welcome.jpg';
+import Modal from 'react-modal';
 
-export const Search = () => {
-  const [input, setInput] = useState({
-    birthDate: '',
-  })
+const Search = () => {
+  const [dob, setDob] = useState('');
+  const [age, setAge] = useState('');
 
-  function currentAge(dob) {
-    var today = new Date();
-    var birthDate = new Date(dob);
-    var age = today.getFullYear - birthDate.getFullYear();
-    var months = today.getMonth() - birthDate.getMonth();
-    if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate()))
-    {
-      age--;
-    }
-    return age;
-  }
+  // const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  // console.log("modalIsOpen===>". modalIsOpen);
+
+  const currentAge = () => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    // debugger;
+    // const ageDate = new Date(age);
+    // const calculateCurrentAge = Math.abs(age.getUTCFullYear() - 1970)
+
+    setAge(age);
+    // setModalIsOpen(true);
+
+    // const age = today.getFullYear - birthDate.getFullYear();
+    // const months = today.getMonth() - birthDate.getMonth();
+    // if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate()))
+    // {
+    //   age--
+    // }
+    // return age;
+  };
+
+  // function handleChange (e) {
+  //   this.setState({dob: e.target.value}, () => {
+  //     console.log(this.state.dob);
+  //   });
+  //   var newAge = {newAge: this.calculateAge(e.target.value)}
+  //   console.log(newAge);
+  //   this.setState({age1: newAge}, () => {
+  //     console.log('Age:', this.state.age1);
+  //   })
+  // }
 
   return (
-    <div className="w3-container w3-light-grey description" style={{height: 700, width: '100%'}}>
-      <Container className="mt-5 mb-5">
+    <div
+      className='w3-container w3-light-grey description'
+      style={{ height: 700, width: '100%' }}
+    >
+      <Container className='mt-5 mb-5'>
         <Row>
-          <Col className="w3-container" >
-            <Image src={Welcome} alt='welcome' rounded fluid/>
+          <Col className='w3-container'>
+            <Image src={Welcome} alt='welcome' rounded fluid />
           </Col>
-          <Col className="w3-container w3-blue w3-round p-5 w3-card-4" >
-            <div className="w3-center">
-              <h2 className="w3-bold mb-3 fs-2">
-                Age Calculator
-              </h2>
-              <p className="mb-5">
-                Are you curious about your current age or simply interested <br/> in
-                knowing how many days, weeks, or months <br/> you've been on this
-                planet, our intuitive interface makes it <br/> easy to find out.
+          <Col className='w3-container w3-blue w3-round p-5 w3-card-4'>
+            <div className='w3-center'>
+              <h2 className='w3-bold mb-3 fs-2'>Age Calculator</h2>
+              <p className='mb-5'>
+                Are you curious about your current age or simply interested{' '}
+                <br /> in knowing how many days, weeks, or months <br /> you've
+                been on this planet, our intuitive interface makes it <br />{' '}
+                easy to find out.
               </p>
             </div>
-            <form className="w3-container">
-              <label className="w3-text-white">
+            <div className='w3-container'>
+              <label className='w3-text-white'>
                 <b>Date of Birth</b>
               </label>
               <input
-                className="w3-input w3-border w3-round-xxlarge mb-3"
-                type="date"
-                defaultValue= {dob}
-                onChange={this.handleChange}
-                name="dateOfBirth"
+                className='w3-input w3-border w3-round-xxlarge mb-3'
+                type='date'
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                name='dateOfBirth'
               />
-
-              <label className="w3-text-white">
-                <b>Current Date</b>
-              </label>
-              <input
-                className="w3-input w3-border w3-round-xxlarge"
-                type="date"
-              />
-
-              <button className="w3-btn w3-teal w3-round-xxlarge mt-3">
+              <button
+                className='w3-btn w3-teal w3-round-xxlarge mt-3'
+                onClick={() => {
+                  currentAge();
+                }}
+              >
                 Calculate
               </button>
-            </form>
+            </div>
           </Col>
         </Row>
       </Container>
+      <Modal>{/* <Compoment /> */}</Modal>
     </div>
   );
 };
+
+export default Search;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './home.css'
-import { Navbar } from'../../components/Navbar/Navbar';
-import { Search } from'../../components/Search/Search';
-import { Results } from'../../components/Results/Results';
-import { Footer } from'../../components/Footer/Footer';
+import React from "react";
+import "./home.css";
+import { Navbar } from "../../components/Navbar/Navbar";
+import Search from "../../components/Search/Search";
+import { Results } from "../../components/Results/Results";
+import { Footer } from "../../components/Footer/Footer";
 
 export const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Navbar />
       <Search />
       <Results />
       <Footer />
     </div>
-  )
-}
+  );
+};
